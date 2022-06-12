0111b66de60b1de7f8743cafc8b5fa2f728b9233
tyranno <eotjdgkdl@gmail.com>
2022-06-13 02:23:51 +0900

    Fix Build Error

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,4 @@
 import { css } from '@emotion/react';
-import { IsometricMenu } from './stories/IsometricMenu';
 
 const testColor = css({
     color: 'green',
@@ -9,7 +8,6 @@ const App = () => {
     return (
         <div>
             Hello, World!saa1 <p css={testColor}>red Color!</p>
-            <IsometricMenu />
         </div>
     );
 };
