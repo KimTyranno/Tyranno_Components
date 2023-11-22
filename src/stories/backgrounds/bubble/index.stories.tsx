315322b3f6b988648d404787bfabcf7dd78f02d2
tyranno <eotjdgkdl@gmail.com>
2023-11-23 03:09:46 +0900

    background 폴더 수정

--- src/stories/backgrounds/bubble/index.stories.tsx
+++ src/stories/backgrounds/bubble/index.stories.tsx
@@ -2,10 +2,10 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { BubbleBackground } from '.';
 
 export default {
-    title: 'components/Backgrounds/BubbleBackground',
+    title: 'components/Backgrounds/Bubble',
     component: BubbleBackground,
 } as ComponentMeta<typeof BubbleBackground>;
 
 const Template: ComponentStory<typeof BubbleBackground> = () => <BubbleBackground />;
 
-export const Background = Template.bind({});
+export const Bubble = Template.bind({});
